7a080f547988ab3f060b99d5250ef2e2fe9e75b8
linyu <972223177@qq.com>
2026-06-11 20:07:12 +0800

    feat(02-03): create DataSourceProvider, HikariDataSourceProvider, and buildSslContext extension
    - DataSourceProvider interface in com.nebula.common.datasource per D-12
    - HikariDataSourceProvider with by lazy initialization, all params from DatabaseConfig
    - JDBC URL uses mysql-connector-j 9.x compatible sslMode=PREFERRED (not deprecated useSSL)
    - SslConfig.buildSslContext() extension: OPENSSL provider, TLSv1.2/1.3, ECDHE+AES-GCM ciphers

--- common/src/main/kotlin/com/nebula/common/config/SslConfig.kt
+++ common/src/main/kotlin/com/nebula/common/config/SslConfig.kt
@@ -1,7 +1,31 @@
 package com.nebula.common.config
 
+import io.grpc.netty.GrpcSslContexts
+import io.netty.handler.ssl.SslContext
+import io.netty.handler.ssl.SslProvider
+import java.io.File
+import java.security.Security
+
 data class SslConfig(
     val enabled: Boolean,
     val certChainPath: String,
     val privateKeyPath: String
 )
+
+fun SslConfig.buildSslContext(): SslContext? {
+    if (!enabled) return null
+    val certChain = File(certChainPath)
+    val privateKey = File(privateKeyPath)
+    return GrpcSslContexts
+        .forServer(certChain, privateKey)
+        .sslProvider(SslProvider.OPENSSL)
+        .protocols("TLSv1.2", "TLSv1.3")
+        .ciphers(
+            listOf(
+                "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+            ),
+            Security.getProvider("SunJSSE")
+        )
+        .build()
+}
